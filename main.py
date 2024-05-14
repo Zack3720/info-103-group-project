379a660608ad725f0c829b11a7aa6942528e55fc
zcrouse <zcrouse@cs.washington.edu>
2024-05-13 23:49:31 -0700

    Made getPost command work, had to revert back to using the '$' prefix command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import api_keys as keys
+import get_reddit_info as reddit
 from discord.ext import commands
 
 def main():
@@ -17,10 +18,29 @@ def main():
         # TODO: modify this code to post at least 7 times
         # await channel.send("Bot started!")
 
-    # Adds a command called hello to the bot. Can be accessed through typing $hello on discord server
-    @client.tree.command(name= "hello", description= "Prints hellow world")
-    async def hello(ctx: discord.Interaction):
-        await ctx.channel.send("Hello world!")
+    # Adds a command called getPost to the bot. Can be accessed through typing /getPost on discord server
+    # @client.tree.command(name= "getpost", description= "Get some number of posts from a subreddit")
+    @client.command()
+    async def getPost(ctx: discord.Interaction, subredditName, postCount):
+        if subredditName == '':
+            await ctx.channel.send("Missing first arguement, subredditName!")
+            return
+        elif postCount == '':
+            await ctx.channel.send("Missing second arguement, subredditName!")
+            return
+        elif not postCount.isdigit():
+            await ctx.channel.send("Second argument must be a number!")
+            return
+        
+        print(type(subredditName))
+        print(postCount)
+
+        submissions = reddit.getRedditSubmissions(subredditName, int(postCount))
+
+        await ctx.channel.send("Here is the titles of the submissions:")
+
+        for submission in submissions:
+            await ctx.channel.send(submission.title)
         return
 
     # Now that we've defined how the bot should work, start running your bot
